8a79a4c5161b1440c685cc1645d10e19ebc5a05b
rick <rickwestdev@gmail.com>
2019-10-01 23:56:57 +0100

    404

--- source/404.blade.php
+++ source/404.blade.php
@@ -1,3 +1,7 @@
+---
+permalink: 404.html
+---
+
 @extends('_layouts.master')
 
 @section('body')
@@ -9,7 +13,7 @@
     <hr class="block w-full max-w-sm mx-auto my-8 border">
 
     <p class="text-xl">
-        Need to update this page? See the <a title="404 Page Documentation" href="https://jigsaw.tighten.co/docs/custom-404-page/"> Jigsaw documentation</a>.
+        😞
     </p>
 </div>
 @endsection
