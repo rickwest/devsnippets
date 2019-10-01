18bc9d53259360a4a16a4185ac4450aeb28e0d37
rick <rickwestdev@gmail.com>
2019-10-02 00:05:13 +0100

    Tweak 404 page

--- source/404.blade.php
+++ source/404.blade.php
@@ -8,12 +8,6 @@ permalink: 404.html
 <div class="flex flex-col items-center mt-32 text-grey-darker">
     <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
-    <h2 class="text-3xl">Page not found</h2>
-
-    <hr class="block w-full max-w-sm mx-auto my-8 border">
-
-    <p class="text-xl">
-        😞
-    </p>
+    <h2 class="text-3xl">Page not found 😞</h2>
 </div>
 @endsection
