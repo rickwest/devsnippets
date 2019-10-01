48d75272d2638d8648f2f7bc8025a5dcb7b95b42
rick <rickwestdev@gmail.com>
2019-10-01 23:42:16 +0100

    Update landing page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,51 +1,50 @@
 @extends('_layouts.master')
 
 @section('body')
-<section class="container max-w-2xl mx-auto px-6 py-10 md:py-12">
-    <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
-        <div class="mt-8">
-            <h1 id="intro-docs-template">{{ $page->siteName }}</h1>
+    <section class="container max-w-2xl mx-auto px-6 py-10 md:py-12">
+        <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
+            <div class="mt-8">
+                <h1 id="intro-docs-template">{{ $page->siteName }}</h1>
 
-            <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
+                <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
 
-            <p class="text-lg">Having to Google the same thing over and over? <br class="hidden sm:block">There's probably a snippet for that!</p>
+                <p class="text-lg">Having to Google the same thing over and over? <br> Sick of searching Stack Overflow for that obscure answer you found the other week? <br class="hidden sm:block">There's probably a snippet for that (and if there isn't, there should be)!</p>
 
-            <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue hover:bg-blue-dark font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+                <div class="flex my-10">
+                    <a href="/snippets/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue hover:bg-blue-dark font-normal text-white hover:text-white rounded mr-4 py-2 px-6">To the snippets 👉</a>
+                </div>
+            </div>
 
-                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-grey-light hover:bg-grey-dark text-blue-darkest font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
+            <img src="/assets/img/logo-large.svg" alt="{{ $page->siteName }} large logo" class="mx-auto mb-6 lg:mb-0 ">
+        </div>
+
+        <hr class="block my-8 border lg:hidden">
+
+        <div class="md:flex -mx-2 -mx-4">
+
+            <div class="mb-8 mx-3 px-2 md:w-1/3">
+                <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
+
+                <h3 class="text-2xl text-blue-darkest mb-0">What Are Snippets?</h3>
+
+                <p>Snippets are those useful bits of code that you only need every so often. A reference for your future self (and others), saving you that infuriating search for that answer that you didn't know you would need again!</p>
+            </div>
+
+            <div class="mb-8 mx-3 px-2 md:w-1/3">
+                <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
+
+                <h3 class="text-2xl text-blue-darkest mb-0">Copy/Paste Driven Development</h3>
+
+                <p>We're only human, we can't remember everything, and now we don't need to! Copy/Paste programming has never felt so good.</p>
+            </div>
+
+            <div class="mx-3 px-2 md:w-1/3">
+                <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
+
+                <h3 class="text-2xl text-blue-darkest mb-0">Contribute</h3>
+
+                <p>Got a helpful snippet that you want to share? Looking for that first Open Source Contribution (perfect for <a href="https://hacktoberfest.digitalocean.com">Hacktoberfest</a>)? Open an issue or create a pull request on <a href="https://github.com/rickwest/devsnippets">Github</a>.</p>
             </div>
         </div>
-
-        <img src="/assets/img/logo-large.svg" alt="{{ $page->siteName }} large logo" class="mx-auto mb-6 lg:mb-0 ">
-    </div>
-
-    <hr class="block my-8 border lg:hidden">
-
-    <div class="md:flex -mx-2 -mx-4">
-        <div class="mb-8 mx-3 px-2 md:w-1/3">
-            <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
-
-            <h3 id="intro-laravel" class="text-2xl text-blue-darkest mb-0">Templating with <br>Laravel's Blade engine</h3>
-
-            <p>Blade is a powerful, simple, and beautiful templating language, and now you can use it for your static sites, not just your Laravel-powered apps.</p>
-        </div>
-
-        <div class="mb-8 mx-3 px-2 md:w-1/3">
-            <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
-
-            <h3 id="intro-markdown" class="text-2xl text-blue-darkest mb-0">Use Markdown for <br>content-driven pages</h3>
-
-            <p>Markdown is the web’s leading format for writing articles, blog posts, documentation, and more. Jigsaw makes it painless to work with Markdown content.</p>
-        </div>
-
-        <div class="mx-3 px-2 md:w-1/3">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
-
-            <h3 id="intro-mix" class="text-2xl text-blue-darkest mb-0">Compile your assets <br>using Laravel Mix </h3>
-
-            <p>Jigsaw comes pre-configured with Laravel Mix, a simple and powerful build tool. Use the latest frontend tech with just a few lines of code.</p>
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
