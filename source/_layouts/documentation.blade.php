690cd1db7da208b1acce54c2d315edc94b4a6d87
Rick West <rickwestdev@gmail.com>
2019-10-03 12:54:50 +0100

    Add DocSearch-content class to content body
    As advised by Algolia

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -11,7 +11,7 @@
             @include('_nav.menu', ['items' => $page->navigation])
         </nav>
 
-        <div class="w-full lg:w-3/5 break-words pb-16 lg:pl-4" v-pre>
+        <div class="DocSearch-content w-full lg:w-3/5 break-words pb-16 lg:pl-4" v-pre>
             @yield('content')
         </div>
     </div>
